d9c2fcb4c294c37116c9cfb4110fad7196f92119
Ivan Usov <ivan.usov@psi.ch>
2022-12-05 15:20:45 +0100

    Revert "Share axis ranges between plots"
    This reverts commit 4de1172b5f7dfea35017506fb212c37b940e923b.

--- app/panel_jitter.py
+++ app/panel_jitter.py
@@ -40,7 +40,6 @@ def create():
     ix_fig = figure(
         x_axis_label="INT",
         y_axis_label="XPOS",
-        y_range=xy_fig.x_range,
         height=300,
         width=500,
         tools="pan,wheel_zoom,save,reset",
@@ -63,8 +62,6 @@ def create():
     iy_fig = figure(
         x_axis_label="INT",
         y_axis_label="YPOS",
-        x_range=ix_fig.x_range,
-        y_range=xy_fig.y_range,
         height=300,
         width=500,
         tools="pan,wheel_zoom,save,reset",
