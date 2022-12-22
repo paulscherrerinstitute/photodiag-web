8264dacd94e981401559ab785afe870c3c69f4cf
Ivan Usov <ivan.usov@psi.ch>
2022-12-22 12:39:04 +0100

    Add photodiag_web entry point

---
+++ photodiag_web/cli.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+import sys
+
+
+def main():
+    app_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app")
+    subprocess.run(["bokeh", "serve", app_path, *sys.argv[1:]], check=True)
+
+
+if __name__ == "__main__":
+    main()
